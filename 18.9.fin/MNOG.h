54b9fef277528d4b4c66d9aa1a0fa072248d3aff
Pin <75661374+mojo-pin@users.noreply.github.com>
2021-06-22 14:26:41 +0500

    Add files via upload

---
+++ 18.9.fin/MNOG.h
@@ -0,0 +1,15 @@
+class Vector
+{
+public:
+	Vector(int s);
+	int give(int index);
+	int givesize();
+	void cross();
+	void end();
+	void del(int i);
+	void print();
+private:
+	int size;
+	int* data;
+	int* data1;
+};
